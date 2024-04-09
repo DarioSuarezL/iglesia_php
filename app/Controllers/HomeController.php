4327118b80747f9e6ceccf2201fd4b5760ea263b
DarioSuarezL <dsuarezlazarte@gmail.com>
2024-04-09 00:15:48 -0400

    Definiendo pre-consultas

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,11 +2,25 @@
 
     namespace App\Controllers;
 
+    use App\Models\MemberModel;
+
     class HomeController extends Controller{
         public function index(){
 
-            return $this->view('home', [
-                'title' => 'Home'
+            $memberModel = new MemberModel();
+
+            return $memberModel->create([
+                'nombre' => 'Juan',
+                'apellido_paterno' => 'Perez',
+                'apellido_materno' => 'Gomez',
+                'fecha_nacimiento' => '1990-01-01',
+                'estado_civil' => 'Soltero',
+                'numero_celular' => 65085391,
+                'email' => 'correo@correo.com',
             ]);
+
+            // return $this->view('home', [
+            //     'title' => 'Home'
+            // ]);
         }
     }
--- app/Models/Model.php
+++ app/Models/Model.php
@@ -10,6 +10,10 @@
         protected $db_pass = DB_PASS;
         protected $db_name = DB_NAME;
         protected $conn;
+        protected $query;
+
+        //Pa que no se maree el intelliphense
+        protected $table;
 
         public function __construct()
         {
@@ -24,4 +28,56 @@
             }
             // echo "Connected successfully <br> PORT:3306";
         }
+
+        public function query($sql){
+            $this->query = $this->conn->query($sql);
+
+            return $this;
+        }
+
+        public function get(){
+            return $this->query->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function first(){
+            return $this->query->fetch_assoc();
+        }
+
+        // consultas prearmadas
+
+        public function all(){
+            $sql = "SELECT * FROM {$this->table}";
+            return $this->query($sql)->get();
+            // return $this;
+        }
+
+        public function find($id){
+            $sql = "SELECT * FROM {$this->table} WHERE id = {$id}";
+            return $this->query($sql)->first();
+        }
+
+        public function where($column, $operator, $value = null){
+            // return $memberModel->where('numero_celular','>' ,65085392)->get();
+            if($value === null){
+                $value = $operator;
+                $operator = '=';
+            }
+            $sql = "SELECT * FROM {$this->table} WHERE {$column} {$operator} '{$value}'";
+            $this->query($sql);
+
+            return $this;
+        }
+
+
+        public function create($data){
+            //INSERT INTO table_name (column1, column2, column3, ...)
+            //VALUES (value1, value2, value3, ...);
+            $columns = implode(',', array_keys($data));
+            $values = "'".implode("','", array_values($data))."'";
+            $sql = "INSERT INTO {$this->table} ({$columns}) VALUES ({$values})";
+
+            $this->query($sql);
+            return $this->conn->insert_id;
+        }
+
     }
