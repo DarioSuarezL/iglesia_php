82660f47321776a52dea99adb3fc992e4f170a70
DarioSuarezL <dsuarezlazarte@gmail.com>
2024-04-10 05:10:42 -0400

    Mas en el sql script

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,10 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo $title ?></title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Hola desde la vista</h1>
-    <h2><?php echo $title ?></h2>
+    <div class="p-3 text-center">
+        <h1>Administrar iglesia</h1>
+        <a href="/members" class="btn btn-primary">Miembros</a>
+        <a href="#" class="btn btn-primary">Ministerios</a>
+        <a href="#" class="btn btn-primary">Certificados de Matrimonio</a>
+        <a href="#" class="btn btn-primary">Certificados de Bautizo</a>
+        <a href="#" class="btn btn-primary">Cargos</a>
+    </div>
+    <!-- <a href="/members/create" class="btn btn-primary">Registrar miembro</a> -->
 </body>
 </html>
